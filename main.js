67e62dea8e71825a79355cfa20d58336ce9cc290
eduardo.cdorado@alumnos.udg.mx <G3ReNc142020>
2021-01-27 16:06:32 -0600

    Template strings

--- main.js
+++ main.js
@@ -1,50 +1,23 @@
-let nombre = 'Lalo'
-let apelli = 'Chavez'
-let saludo = 'Hola mundo'
-let lorem = 'Lorem ipsum dolor sit id voluptates quod corrupti beatae temporibus.'
-
-
-
-console.log(nombre, apelli, saludo);
-
-console.log(nombre.length, apelli.length, saludo.length);
-
-console.log(nombre.toUpperCase());
-console.log(apelli.toLowerCase());
-console.log(lorem.includes('sit'));
-
-
 /* 
-    .length
-    Para saber la cantidad de caracteres
-    que hay dentro de un string
-
-    .toUpperCase() 
-    Para convertir el texto a mayusculas
-
-    .toLowerCase()
-    Para convertir el texto en minusculas
-
-    .includes('lalo')
-    Para buscar una letra, palabra o parrafo
-    en una variable o string
-
-    .trim()
-    Quita los espacios en blanco, al 
-    inicio y al final de la variable 
-
-    .split("")
-    Podemos convertuir un texto a un
-    arreglo, separado automaticamente 
-    por comas
-
-    Propiedades:
-    Caracteristicas
-
-    Metodos:
-    Acciones o funciones para 
-    realizar una accion
-
-
+    Interpolacion de variables
 */
 
+let nombre = 'Lalo'
+let apelli = 'Chavez'
+let saludo = `Hola mi nombre es ${nombre} y mi apellido es ${apelli}`
+
+console.log(saludo);
+
+/*  Template string ↓ 
+    imrpime con todo y los
+    saltos de linea
+*/
+
+let ul = `
+<ul>
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>`;
+
+console.log(ul);
