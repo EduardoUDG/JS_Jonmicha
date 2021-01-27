d51dc44566f4ac180e66a94a554edfbe25a90e3c
eduardo.cdorado@alumnos.udg.mx <G3ReNc142020>
2021-01-27 15:48:01 -0600

    cadenas de texto (string) dentro del main.js

---
+++ main.js
@@ -0,0 +1,50 @@
+let nombre = 'Lalo'
+let apelli = 'Chavez'
+let saludo = 'Hola mundo'
+let lorem = 'Lorem ipsum dolor sit id voluptates quod corrupti beatae temporibus.'
+
+
+
+console.log(nombre, apelli, saludo);
+
+console.log(nombre.length, apelli.length, saludo.length);
+
+console.log(nombre.toUpperCase());
+console.log(apelli.toLowerCase());
+console.log(lorem.includes('sit'));
+
+
+/* 
+    .length
+    Para saber la cantidad de caracteres
+    que hay dentro de un string
+
+    .toUpperCase() 
+    Para convertir el texto a mayusculas
+
+    .toLowerCase()
+    Para convertir el texto en minusculas
+
+    .includes('lalo')
+    Para buscar una letra, palabra o parrafo
+    en una variable o string
+
+    .trim()
+    Quita los espacios en blanco, al 
+    inicio y al final de la variable 
+
+    .split("")
+    Podemos convertuir un texto a un
+    arreglo, separado automaticamente 
+    por comas
+
+    Propiedades:
+    Caracteristicas
+
+    Metodos:
+    Acciones o funciones para 
+    realizar una accion
+
+
+*/
+
