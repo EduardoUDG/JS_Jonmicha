82ba3bd018782cb0670a00bddcaa56b42b30c237
eduardo.cdorado@alumnos.udg.mx <G3ReNc142020>
2021-01-28 17:29:02 -0600

    Numeros (Numbers)

--- main.js
+++ main.js
@@ -1,23 +1,35 @@
+let a = 2
+let b = new Number(1)
+let c = 7.19
+let d = '2.6'
+
+
+console.log(a, b, c.toFixed(1));
+console.log(parseInt(c));
+console.log(parseInt(d));
+console.log(typeof c, typeof d)
+console.log(c + parseInt(d))
+console.log(c + parseFloat(d))
+
 /* 
-    Interpolacion de variables
-*/
+    .toFixed(1)
+    Esta funcion nos ayuda a redondear un 
+    un alor numerico con numero decimal
+    indicando cuantas a decimas se reducira
+ 
 
-let nombre = 'Lalo'
-let apelli = 'Chavez'
-let saludo = `Hola mi nombre es ${nombre} y mi apellido es ${apelli}`
+    parseInt(nom_variable)
+    Esta funcion nos regresa el valor entero
+    de una varible
 
-console.log(saludo);
+    parseFloat(nom_varible)
+    Imprime el valor entero como la parte 
+    decimal de una varible
 
-/*  Template string ↓ 
-    imrpime con todo y los
-    saltos de linea
-*/
+    typeof nom_variable
+    Nos regresa el tipo de dato que 
+    es la variable 
 
-let ul = `
-<ul>
-    <li></li>
-    <li></li>
-    <li></li>
-</ul>`;
+ */
 
-console.log(ul);
+    
