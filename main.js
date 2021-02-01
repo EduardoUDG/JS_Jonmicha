e24d913f141666c0197b8e3dff1abb95f26603c9
Eduardo Chavez <eduardo.cdorado@alumnos.udg.mx>
2021-02-01 15:23:00 -0600

    Arreglos y algunos metodos

--- main.js
+++ main.js
@@ -1,70 +1,42 @@
 /* 
-    Las funciones son poderoas en js porque
-    les podemos asignar como valor, tambien puede 
-    retornar la ejecucion de una funcion adicional
-    como argumentos, retornar como valores
+    Tenemos diferentes maneras de hacer arreglos:
+*/
+
+// Primera forma
+const a = []
+const b = ['A', true, 'hola', ['1', '2', '3']]
+//  .length nos devuelve el (Numero de elementos de un rarreglo)
+console.log(a.length);
+console.log(b.length);
+
+
+
+//  Segunda forma
+const c = Array.of('x', 'Y', 'z', 9, 8, 7);
+console.log(c);
+
+
+/* ------------------------------------------------------------- */
+// Podemos inicializar un arreglo de la siguiente manera
+// utilizando el prototipo de un arreglo como se muestra:
+
+/* 
+const d = Array(50).fill(false);
+console.log(d);
 */
 
 
-// Funcion Declarada
-function sumar(a, b) {
-    return a+b;
-}
 
-// Invocación de Funcion ↓
-let pruebaFuncion = sumar(3,3);
-/* console.log(pruebaFuncion);*/
-
-
-/* -------------------------------------------------- */
+const colores = ['Rojo', 'Azul', 'Verde']
+console.log(colores);
+// .push manda un nuevo elemento al final
+colores.push('Amarillo')
+console.log(colores);
+// .pop quita el ultimo elemento del areglo
+colores.pop();
+console.log(colores);
 
 
-
-// Declarando valor por defecto en 
-// una funcion con parametros
-function saludo(nombre = 'user', edad='0') {
-
-    if (nombre == 'user'){
-        console.log(`Hola ${nombre}, Bienvenido`);        
-    } else {
-        console.log(`Hola ${nombre} tu edad es ${edad} años`);
-    }
-}
-// saludo('Lalo', 20);
-// saludo();
-
-
-
-/* -------------------------------------------------- */
-
-
-
-// Funciones Declaradas VS Funciones Expresadas
-// funcionDeclarada()
-
-function funcionDeclarada() {
-    console.log('Esta es una funcion declarada');
-}
-
-// funcionDeclarada()
-
-
-
-/* -------------------------------------------------- */
-
-// Fnciones con expresiones
-// crear una funcion y asignarsela dinamicamente a una variable
-
-
-
-// No podemos llamar este tipo de funcion si no a sido primero declarada  
-// Funcion Anonima ↓ (Se llama asi, por que la funcion no tiene nombre)
-
-const funcionExpresada =  function() {
-    console.log('Esta es una funcion expresada');    
-}
-
-funcionExpresada(); 
-
-
-
+colores.forEach(function (el, i){
+    console.log(`<li id="${i}"> ${el} </li> `)
+});
