0011a0f3135f40c9fa5ec67ba19e35e2c50a2ffe
Eduardo Chavez <eduardo.cdorado@alumnos.udg.mx>
2021-02-03 15:46:16 -0600

    Ciclos Forin y Forof (lopos)

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
 /* 
-    * Operadores
-    TODO *, +, -, /, %
-    
-    * Condicionales
-    TODO  <, >, >=, <=, =, ==, ===, !==, !=, !==  
-      
-    * Operador unario
-    TODO i++, ++i, i--, --i
-
-    * Operadores logicos
-    TODO ||, &&, !
-
+*   Un forin nos permite recorrer todas
+*   las propiedades de un objeto: 
 */
 
-let a = '10'
-console.log(typeof a, a );
+const user = {
+    nameUser: 'Eduardo',
+    lastUser: 'Chavez',
+    ageUser: 20,
+}
 
 
-a = parseInt(a);
-console.log(typeof a, a );
+for (const propiedad in user) {
+    console.log(propiedad);
+    console.log(`Key: ${propiedad}, Values: ${user[propiedad]}`);
+}
+
+/* 
+*   Un forof nos permite recorrer todos los
+*   elementos de cualquier objeto que sea
+*   iterable en JavaScrit
+*/
+
+const numeros = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+for (const elemento of numeros) {
+    console.log(elemento);
+}
