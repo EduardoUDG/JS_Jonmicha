5922f90eecffd58e9bef4b9294746f47e64a91c9
eduardo.cdorado@alumnos.udg.mx <G3ReNc142020>
2021-02-17 10:44:34 -0600

    Objeto console y más (time, error, info, warm, clear, dir, group, table, assert)

--- main.js
+++ main.js
@@ -1,81 +1,121 @@
 
-class Animal {
-    // * El constructor es un metodo especial que se ejecuta
-    // * en el momento de instanciar la clase
-    constructor(nombre, genero){
-        // Atributos
-        this.nombre = nombre;
-        this.genero = genero;
-    }
+console.log(console);
+console.error('Esto es un error');
+console.warn('Esto es un aviso');
+console.info('Este es un mensaje info');
 
-    // Metodos
-    sonar(){
-        console.log('Hago sonidos por que estoy vivo');
-    }
-    saludar(){
-        console.log(`Hola me llamo ${this.nombre}`);
-    }
+// * log es como un registro de lo que  
+// * a pasado en nuestra aplicacion
+
+let nombre = 'Eduardo', apellido = 'Chavez', edad = 20;
+
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+console.log(nombre, apellido, edad);
+console.log(`Hola mi nombre es ${nombre} ${apellido} y tengo ${edad}`);
+
+
+// * Comodines 
+console.log(`Hola mi nombre es %s %s y tengo %d`, nombre, apellido, edad);
+console.clear();
+// console.log(window);
+
+
+// * Imprime lo que hay en el documento
+// * por ejemplo imprimiendo las etiquetas HTML
+// console.log(document);
+
+
+// * (dir) muestra las propiedades y metodos en formato objeto
+// console.dir(document);t
+console.clear();
+
+console.group('Este es un group dentro la consola');
+console.log('Item #1');
+console.log('Item #2');
+console.log('Item #3');
+console.log('Item #4');
+console.log('Item #5');
+console.groupEnd();
+
+
+
+// console.log(console);
+
+/* 
+* Queremos que represente todas las entradas
+* Del objeto console en una tabla
+* Usando el constructor objetc
+* y el metodo entries, para que genere una 
+* Fila por todas las propiedades del objeto console
+*/
+
+// console.table(Object.entries(console));
+// console.table(Object.entries(console).sort());
+
+
+
+
+
+
+
+console.clear();
+const numeros2 = [1, 2, 3, 4, 5]
+const vocales = ['a','e','i','o','u'];
+console.table(numeros2); 
+console.table(vocales);
+
+
+
+
+
+
+
+// * Tabla con objetos
+console.clear();
+
+const perro = {
+    nombre: 'Pedro',
+    raza: 'Pug',
+    color: 'Dorado',
+} 
+
+const array1 = ['Juan','Carlos','Pepe','Diego','Ramirez','Javier','Luis'];
+// console.table(perro);
+
+
+
+console.time('Cuanto tiempo tarda mi código');
+const arreglo2 = Array(1000000);
+for (let i = 0; i < arreglo2.length; i++) {
+    arreglo2[i] = i;    
 }
-
-
-const mimi = new Animal('Mimi','Hembra');
-const scooby = new Animal('Scooby','Macho');
-
-console.log(mimi);
-console.log(scooby);
+console.timeEnd('Cuanto tiempo tarda mi código');
 
 
 
-//  * -------------------------------------------------------
-//  * HERENCIA 
+// * ----------------------------------------
+console.clear();
+// * Nos ayuda a saber cuantas veces 
+// * Se realizo una tarea, ciclo ect
 
-class Perro extends Animal {
+// for (let i = 0; i < 100; i++) {
+//     console.count('Código for');
+//     console.log(i);
+// }
 
-    constructor(nombre, genero, tamanio) {
-        // * Con el metodo super() se manda a
-        // * llamar el constructor de la clase padre
-        super(nombre, genero);
-        this.tamanio = tamanio;
-        this.raza = null;
-    }
 
-    sonar() {
-        console.log('Soy un perro y mi sonido es un ladrido');
-    }
 
-    ladrar(){
-        console.log('Guauu Guauu!');
-    }
+// * ------ una forma de hacer test muy basico
 
-    // * Un metodo estático se puede ejecutar  
-    // * sin necesidad instanciar la clase
+let x = 1
+let y = 2
+let pruebaXY = 'Se esperaba que X siempre esa menor que Y';
 
-    static soyMetodoEstatico(){
-        console.log('Hola soy un metodo estático');
-    }
+console.assert(x < y, {x, y ,pruebaXY}); 
 
-    // * Los setters y getters son métodos especiales 
-    // * que nos permiten establecer y obtener los
-    // * valores de atributos de nuestra clase
-    
-    get getRaza(){
-        return this.raza;
-    }
-    set setRaza(raza){
-        this.raza = raza;
-    }
 
-}
-// ! NOTA cuando usamos set y get
-// ! JS convierte las funciones en propiedades
 
-Perro.soyMetodoEstatico();
 
-const scooby2 = new Perro ('Scooby', 'Macho', 'Gigante');
 
-console.log(scooby2);
-scooby2.saludar();
-scooby2.sonar();
-console.log(scooby2.getRaza);
-scooby2.setRaza = 'Grán danés';
-console.log(scooby2.getRaza);
