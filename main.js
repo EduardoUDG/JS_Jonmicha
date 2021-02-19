d83376f3bff4f92357474366914ca55dce4de378
eduardo.cdorado@alumnos.udg.mx <G3ReNc142020>
2021-02-19 11:41:38 -0600

    Operador de conrtocircuito (OR, AND)

--- main.js
+++ main.js
@@ -1,38 +1,40 @@
-// ? Podemos ver a math como una libreria
-// ? ya que tiene funciones y valores constantes
-// ? ya definidos como las c y c++
+
+/* 
+* Cortocuito OR / cuando el valor de la izquierda en la
+* expresion siempre puede validar a true, es el valor que
+* se cargara por defecto
+*/
+
+/* 
+* Cortocuito AND / cuando el valor de la izquierda en la
+* expresion siempre puede validar a false, es el valor 
+* que se cargará por defecto
+*/
 
 
-// * Obejto math
-console.log(Math);
 
-// * Podemos usar math para sacar valores CONSTANTES 
-console.log('Valor constante' ,Math.PI);
+function saludar(nombre) {
+    // ! Cortocircuito
+    nombre = nombre || 'Desconocido';
+    console.log(`Hola ${nombre}`);
+}
 
-// * Valor absoluto
-console.log('Absoluto',Math.abs(-7.8));
+saludar('Eduardo');
+saludar();
+console.log('Cadena' || 'Valor de la derecha');
+console.log( 19 || 'Valor de la derecha');
+console.log( true || 'Valor de la derecha');
+console.log( [] || 'Valor de la derecha');
+console.log( {} || 'Valor de la derecha');
+console.log( false || 'Valor de la derecha');
+console.log( null || 'Valor de la derecha');
+console.log( undefined || 'Valor de la derecha');
+console.log( '' || 'Valor de la derecha');
+console.log( 0 || 'Valor de la derecha');
 
-// * Unidad arriba
-console.log('Entero hacia arriba 7.8 = ' ,Math.ceil(7.8));
-
-// * Unidad abajo
-console.log('Entero hacia abajo 7.2 = ' ,Math.floor(7.4));
-
-// * Redondear
-console.log('Redondear 7.5 = ' ,Math.round(7.5));
-console.log('Redondear 7.4 = ' ,Math.round(7.4));
-
-// * Raiz
-console.log('Raiz de 81 = ' ,Math.sqrt(81));
-
-// * Potencia
-console.log('Potencia de 2^3 = ' ,Math.pow(2,3));
-
-// * Saber si un numero es positivo o negativo
-console.log('Positivo = 0, Negativo = -1, cero = 0' ,Math.sign(-10));
-console.log('Positivo = 0, Negativo = -1, cero = 0' ,Math.sign(0));
-console.log('Positivo = 1, Negativo = -1, cero = 0' ,Math.sign(10));
-
-// * Random, simpre da un numero aleatorio entre 0 y 1
-console.log('Numero Random = ' , Math.round(Math.random() * 1000));
+console.log( false && 'Valor de la derecha');
+console.log( null && 'Valor de la derecha');
+console.log( undefined && 'Valor de la derecha');
+console.log( '' && 'Valor de la derecha');
+console.log( 0 && 'Valor de la derecha');
 
