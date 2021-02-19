ec969ff197feb0bba51cbd3260eb88142ee19ea8
eduardo.cdorado@alumnos.udg.mx <G3ReNc142020>
2021-02-19 12:04:44 -0600

    alert, confirm y prompt

--- main.js
+++ main.js
@@ -1,40 +1,15 @@
 
-/* 
-* Cortocuito OR / cuando el valor de la izquierda en la
-* expresion siempre puede validar a true, es el valor que
-* se cargara por defecto
-*/
+// console.log(window);
 
-/* 
-* Cortocuito AND / cuando el valor de la izquierda en la
-* expresion siempre puede validar a false, es el valor 
-* que se cargará por defecto
-*/
+// alert('Hola esto es una alerta');
+// confirm('Hola esto es una alerta');
+// prompt('Hola esto es un aviso y le permite al usuario ingresar un valor');
 
-
-
-function saludar(nombre) {
-    // ! Cortocircuito
-    nombre = nombre || 'Desconocido';
-    console.log(`Hola ${nombre}`);
-}
-
-saludar('Eduardo');
-saludar();
-console.log('Cadena' || 'Valor de la derecha');
-console.log( 19 || 'Valor de la derecha');
-console.log( true || 'Valor de la derecha');
-console.log( [] || 'Valor de la derecha');
-console.log( {} || 'Valor de la derecha');
-console.log( false || 'Valor de la derecha');
-console.log( null || 'Valor de la derecha');
-console.log( undefined || 'Valor de la derecha');
-console.log( '' || 'Valor de la derecha');
-console.log( 0 || 'Valor de la derecha');
-
-console.log( false && 'Valor de la derecha');
-console.log( null && 'Valor de la derecha');
-console.log( undefined && 'Valor de la derecha');
-console.log( '' && 'Valor de la derecha');
-console.log( 0 && 'Valor de la derecha');
+let alerta = alert('Hola esto es una alerta');
+let confirmacion = confirm('Hola esto es una alerta');
+let aviso = prompt('Hola esto es un aviso y le permite al usuario ingresar un valor');
+ 
+console.log(alerta);
+console.log(confirmacion);
+console.log(aviso);
 
